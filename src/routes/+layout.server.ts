363e9bb5e7a57d462ca903d6b3d74e8405948ad4
Julian Hammer <jh@realgolf.games>
2024-11-21 20:17:15 +0000

    fix: update redirect URL in layout server to point to the new domain

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
-import { PUBLIC_YOUR_DOMAIN } from '$env/static/public';
 import type { ServerLoad } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 
 export const load: ServerLoad = async () => {
-	throw redirect(307, PUBLIC_YOUR_DOMAIN);
+	throw redirect(307, 'https://realgolf.games');
 };
